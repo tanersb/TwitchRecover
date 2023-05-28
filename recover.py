5cd07bc31f21d143169572485ab99153493aed1e
owk880301 <134603454+owk880301@users.noreply.github.com>
2023-05-28 17:18:43 +0800

    Update recover.py

--- recover.py
+++ recover.py
@@ -5,11 +5,8 @@ import urllib.request
 from threading import Thread
 from bs4 import BeautifulSoup
 import requests
-import webbrowser
-import cfscrape
-import cloudscraper
 
-api_key = "YOUR_SCRAPINGANT_API_KEY"
+api_key = "YOUR_API_KEY"
 
 domains = [
     "https://vod-secure.twitch.tv",
@@ -85,10 +82,7 @@ def linkTimeCheck(link):
         try:
             time = gelenveri[0].text
         except:
-            print('You probably got into cloudflare for bots.(could not find time data) There is nothing I can do for this error for now. \n'
-                  'Please fork if you can bypass this cloudflare. \n'
-                  'You will not get an error when you try again after a while. \n'
-                  'So try again after a while. ')
+            print('It seems you even copy and paste your api key, please paste it and try again. ')
             return
 
         if '\n' in time:
@@ -193,8 +187,11 @@ def linkTimeCheck(link):
 
         soup = BeautifulSoup(r.content, 'html.parser')
 
-        meta_element = soup.find("meta", attrs={"name": "description"})
-
+        try:
+            meta_element = soup.find("meta", attrs={"name": "description"})
+            content = meta_element.get("content")
+        except AttributeError:
+            print("It seems you even copy and paste your api key, please paste it and try again.")
         content = meta_element.get("content")
         first_time = content.split(" - ")[0].split("on ")[1]
         
